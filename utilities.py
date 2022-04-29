c23c65a9f4c2701728528197e73d0ca9fd6f4514
innocentmunai <innocentmunai@gmail.com>
2022-04-29 15:04:56 -0400

    moved functions to utilities

---
+++ utilities.py
@@ -0,0 +1,122 @@
+import numpy as np
+import cv2
+
+
+
+face_cascade = cv2.CascadeClassifier('detectors/haarcascade_frontalface_default.xml')
+eye_cascade = cv2.CascadeClassifier('detectors/haarcascade_eye.xml')
+
+
+def detect_faces(opened_image):
+    """
+    ????
+    """
+    new_img = np.array(opened_image.convert("RGB"))
+    faces = face_cascade.detectMultiScale(new_img, 1.1, 6)
+
+    # Draw rectangles around the faces
+    for (x, y, w, h) in faces: #4 corners
+        cv2.rectangle(new_img, (x,y), (x+w, y+h), (255, 0, 0), 2) #2 border
+    return new_img, faces
+
+
+def detect_eyes(opened_image):
+    """
+    ????
+    """
+    new_img = np.array(opened_image.convert("RGB"))
+    eyes = eye_cascade.detectMultiScale(new_img, 1.3, 5)
+
+    # Draw rectangles around eyes
+    for (x, y, w, h) in eyes:  # 4 corners
+        cv2.rectangle(new_img, (x, y), (x+w, y+h), (0, 255, 0), 2)  # 2 border
+    return new_img, eyes
+
+
+kelvin_table = {
+    1000: (255,56,0),
+    1500: (255,109,0),
+    2000: (255,137,18),
+    2500: (255,161,72),
+    3000: (255,180,107),
+    3500: (255,196,137),
+    4000: (255,209,163),
+    4500: (255,219,186),
+    5000: (255,228,206),
+    5500: (255,236,224),
+    6000: (255,243,239),
+    6500: (255,249,253),
+    7000: (245,243,255),
+    7500: (235,238,255),
+    8000: (227,233,255),
+    8500: (220,229,255),
+    9000: (214,225,255),
+    9500: (208,222,255),
+    10000: (204,219,255)}
+
+
+def temp(opened_image, k):
+    """
+    ?????
+    """
+    r, g, b = kelvin_table[k]
+    temp_matrix = (r / 255.0, 0.0, 0.0, 0.0,
+                   0.0, g / 255.0, 0.0, 0.0,
+                   0.0, 0.0, b / 255.0, 0.0)
+    temp_image = opened_image.convert('RGB', temp_matrix)
+    return temp_image
+
+
+def cartoon(opened_image):
+    """
+    ?????
+    """
+    new_img = np.array(opened_image.convert("RGB"))
+    edge = cv2.bitwise_not(cv2.Canny(new_img, 200, 300))
+    smooth = cv2.edgePreservingFilter(
+        new_img, flags=2, sigma_s=64, sigma_r=0.3)
+    cartoon_img = cv2.bitwise_and(smooth, smooth, mask=edge)
+    return cartoon_img
+
+
+def cannize(opened_image):
+    """
+    ?????
+    """
+    new_img = np.array(opened_image.convert("RGB"))
+    img = cv2.GaussianBlur(new_img, (15, 15), 0)
+    cannized_img = cv2.Canny(img, 100, 150)
+    return cannized_img
+
+
+def sepia(opened_image):
+    """
+    ?????
+    """
+    new_img = np.array(opened_image.convert("RGB"), dtype=np.float64)
+    sepia_matrix = np.matrix([[0.390, 0.769, 0.189],
+                              [0.349, 0.686, 0.168],
+                              [0.272, 0.534, 0.131]])
+    sepia_img = cv2.transform(new_img, sepia_matrix)
+    sepia_img[np.where(sepia_img > 255)] = 255
+    sepia_img = np.array(sepia_img, dtype=np.uint8)
+    return sepia_img
+
+
+def pencil(opened_image):
+    """
+    ?????
+    """
+    new_img = np.array(opened_image.convert("RGB"))
+    sk_gray, sk_color = cv2.pencilSketch(
+        new_img, sigma_s=60, sigma_r=0.07, shade_factor=0.1)
+    return sk_gray, sk_color
+
+
+def inv(opened_image):
+    """
+    ?????
+    """
+    new_img = np.array(opened_image.convert("RGB"))
+    inv_img = cv2.bitwise_not(new_img)
+    return inv_img
